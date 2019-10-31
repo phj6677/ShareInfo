f43107828bce4a76a96d4c16aa9232d329e8113a
박현조 <hyunjo.park@innowireless.com>
2019-10-31 18:18:37 +0900

    add iOS project

---
+++ iOS/ShareInfo/ShareInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareInfo
+//
+//  Created by macowner on 2019. 10. 31..
+//  Copyright © 2019년 macowner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
